c225c7475ed5978352b6592e01a21579d699384c
JooIcksoo <dlrtn751@gmail.com>
2024-11-01 09:43:15 +0900

    initial commit

---
+++ src/test/java/dlrtn/back_spring/example/ExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dlrtn.back_spring.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
